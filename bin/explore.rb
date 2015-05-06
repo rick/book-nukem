de006cff9a2ed9bcbe1aed3410cf871709445fff
Rick Bradley <rick@rickbradley.com>
2015-05-05 22:32:58 -0500

    iterate over all posts, summarizing

--- bin/explore.rb
+++ bin/explore.rb
@@ -4,21 +4,43 @@ require 'koala'
 require 'yaml'
 require 'pp'
 
-# read ~/.facebook.yml
-
-creds = YAML.load(File.read(File.expand_path('~/.facebook.yml')))
-access_token = creds["access_token"]
-
-graph = Koala::Facebook::API.new(access_token)
-
-me = graph.get_object("me")
-
-
-feed = graph.get_connections("me", "feed")
-while feed
-  feed.each do |post|
-    pp post
-  end
-  feed = feed.next_page
-  puts "end of page ...\n\n\n"
+def display_post(post, post_count)
+  likes = post["likes"] ? post["likes"]["data"] : []
+  comments = post["comments"] ? post["comments"]["data"] : []
+  puts %Q{POST #{post_count}: #{post["created_time"]} [#{post["id"]}] "#{post["description"]}"}
+  puts "      #{likes.size} likes, #{comments.size} comments"
+  puts
 end
+
+def tally_post(post)
+  likes = post["likes"] ? post["likes"]["data"] : []
+  comments = post["comments"] ? post["comments"]["data"] : []
+  [ likes.size, comments.size ]
+end
+
+def iterate_over_posts(graph)
+  feed = graph.get_connections("me", "feed")
+  post_count = likes_count = comments_count = 0
+  while feed
+    feed.each do |post|
+      post_count += 1
+      likes, comments = tally_post post
+      likes_count += likes
+      comments_count += comments
+
+      display_post post, post_count
+    end
+    feed = feed.next_page
+  end
+
+  puts "A total of #{post_count} posts, with #{likes_count} likes and #{comments_count} comments."
+end
+
+def initialize_graph
+  # read ~/.facebook.yml
+  creds = YAML.load(File.read(File.expand_path('~/.facebook.yml')))
+  access_token = creds["access_token"]
+  graph = Koala::Facebook::API.new(access_token)
+end
+
+iterate_over_posts initialize_graph
