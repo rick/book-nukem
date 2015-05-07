29d9c7f31a96fc3a024d6777c32d5544d5307839
Rick Bradley <rick@rickbradley.com>
2015-05-06 23:32:29 -0500

    make the command-line thingy more nicer.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 
 gem "koala", "~> 2.0"
 gem "fql", "~> 0.3"
+gem "commander", "~> 4.0"
--- bin/explore.rb
+++ bin/explore.rb
@@ -4,13 +4,35 @@ $:.unshift(File.expand_path(File.join(File.dirname(__FILE__), "..", "lib")))
 
 require 'pp'
 require 'graph'
+require 'commander/import'
 
-type = ARGV.shift or raise "Usage: #{$0} <type>"
+program :version, '0.0.1'
+program :description, 'explore or delete facebook data'
 
-counter = 0
+command :list do |c|
+  c.syntax = ' list <type>'
+  c.summary = 'list objects of a certain type, e.g., "posts", "inbox", ...'
+  c.description = 'This will list all instances of a given type of object'
 
-results = Graph.new.iterate(type) do |item|
-  puts "I found one!"
-  counter += 1
-  break if counter > 5
+  c.option '--max COUNT', Integer, "Maximum number of records to output"
+
+  c.example 'showing posts', %Q|
+    $ bundle exec ruby bin/explore.rb list posts
+    {"id"=>"123456609586309540",
+     "from"=>{"id"=>"349853749583749", "name"=>"Rick Bradley"},
+     "message"=>
+      "> Three-year program, Journey to Social Inclusion, led to 75% of participants staying in stable housing, and an 80% drop in need for health services"
+      # ...
+  |
+
+  c.action do |args, options|
+    type = args.shift or raise "a type is required. See --help"
+
+    counter = 0
+    results = Graph.new.iterate(type) do |item|
+      pp item
+      counter += 1
+      break if options.max && counter >= options.max
+    end
+  end
 end
